cbcc6c5fad3046bb5a1888bb72605925138f595a
Duy Hoang Ung <duyhoangung@Duys-MacBook-Pro.local>
2024-08-22 15:03:09 -0700

    title & query

--- movieclips_scraping.py
+++ movieclips_scraping.py
@@ -14,6 +14,10 @@ patterns = {
 	"screenwriter": r'SCREE(N)?WRITER(S)?:\s*(\n|\\n)?(.*?)\s*(\n|\\n)'
 }
 
+title_pattern = r'^(.*?) \((\d{4})\)$'
+query_pattern_1 = r'^(.*?) Scene$'
+query_pattern_2 = r'^(.*?) Scene \((\d+)\/(\d+)\)'
+
 list_type = {"cast", "producer", "director", "screenwriter"}
 
 def title_parsing(full_title):
@@ -32,7 +36,7 @@ def potential_clip_description(descriptions):
 				j -= 1
 			if descriptions[j - 1 : j + 1] == '\\n':
 				j -= 2
-			while j >= 0 and descriptions[j] != '\n' and descriptions[j - 1 : j + 1] != '\\n' and descriptions[j - 1 : j + 1] != "='" and descriptions[j - 1 : j + 1] != '="' and descriptions[j - 1 : j + 1] != '"':
+			while j >= 0 and descriptions[j] != '\n' and descriptions[j] != ':' and descriptions[j - 1 : j + 1] != '\\n' and descriptions[j - 1 : j + 1] != "='" and descriptions[j - 1 : j + 1] != '="' and descriptions[j - 1 : j + 1] != '"':
 				if descriptions[j - 5] == '\\' and descriptions[j - 4] == 'u':
 					clip_description = bytes(r"\u" + descriptions[j - 3 : j + 1], "utf-8").decode("unicode-escape") + clip_description
 					j -= 6
@@ -69,33 +73,30 @@ def movieclips_scraping():
 				title_to_url[line.split(' | Movieclips: ')[0]] = line.split(' | Movieclips: ')[1]
 			except:
 				pass
-
+			
 		for full_title in title_to_url:
 			json_dict = dict()
 			if os.path.exists(json_file):
 				with open(json_file, 'r') as file:
 					json_dict = json.load(file)
-			
+		
 			if full_title.replace('/', '_') in json_dict:
 				continue
 
 			if len(json_dict) % 40 == 39:
 				driver.quit()
-				driver = webdriver.Chrome()
-
+				driver = webdriver.Chrome()		
+        
 			url = title_to_url[full_title].split('\n')[0]
 			id = url.split('?')[1].split('&')[0][2:]
 
 			driver.get('{}/videos?view=0&sort=p&flow=grid'.format(url))
 			content = driver.page_source.encode('utf-8').strip()
 			descriptions = str(BeautifulSoup(content, 'lxml'))
-
+			
 			title, query = title_parsing(full_title)
 			full_title = full_title.replace('/', '_')
-
-			json_dict[full_title] = {"id": id, "year": year, "url": url, "title": title, "query": query,
-							"clip_description": '', "film_description": '', "copyright": '',
-							"cast": [], "director": [], "screenwriter": []}
+			json_dict[full_title] = {"id": id, "year": year, "url": url}
 
 			for pattern in patterns:
 				all = re.findall(patterns[pattern], descriptions, re.IGNORECASE)
@@ -104,8 +105,41 @@ def movieclips_scraping():
 						json_dict[full_title][pattern] = clean(max(all[0], key=len)).split(', ')
 					else:
 						json_dict[full_title][pattern] = clean(max(all[0], key=len))
-				elif pattern == 'clip_description':
-					json_dict[full_title][pattern] = potential_clip_description(descriptions)
+				else:
+					if pattern == 'clip_description':
+						json_dict[full_title][pattern] = potential_clip_description(descriptions)
+					elif pattern in list_type:
+						json_dict[full_title][pattern] = []
+					else:
+						json_dict[full_title][pattern] = ''
+				
+			match = re.match(title_pattern, title)
+			if match:
+				group = match.groups()
+				title, release_year = group[0].strip(), group[1]
+			else:
+				title = title.strip()
+				match = re.match(title_pattern, json_dict[full_title]["copyright"])
+				if match:
+					group = match.groups()
+					release_year = group[1]
+			display_title = f'{title} {release_year}'
+			match = re.match(query_pattern_2, query)
+			if match:
+				group = match.groups()
+				query, scene, total_scene = group[0].strip(), group[1], group[2]
+				title = f'{title} ({scene}/{total_scene}) - {release_year}'
+			else:
+				match = re.match(query_pattern_1, query)
+				if match:
+					group = match.groups()
+					query = group[0].strip()
+				else:
+					query = query.strip()
+				title = f'{title} - {release_year}'
+			json_dict[full_title]["title"] = title
+			json_dict[full_title]["query"] = query
+			json_dict[full_title]["display_title"] = display_title
 
 			with open(json_file, 'w', encoding="utf-8") as file:
 				json.dump(json_dict, file, indent=3, ensure_ascii=False)
